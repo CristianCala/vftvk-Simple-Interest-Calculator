79e0bf5da44f18c9e10ce1cce7d1a71b028e1726
Cristian Cala <cristiancala@protonmail.com>
2021-05-13 22:22:12 -0400

    Primer commit

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function updateRate(){
+
+	var rateval = document.getElementById("rate").value;
+	document.getElementById("rate_val").innerText=rateval;
 }
-        
+
+function compute(){
+
+	var principal = document.getElementById("principal").value;
+
+	var rate = document.getElementById("rate").value;
+
+	var years = document.getElementById("years").value;
+
+	var year = new Date().getFullYear()+ parseInt(years);
+
+	var interest = principal * years * rate / 100;
+
+	document.getElementById("result").innerHTML=
+	"If you deposit "+ "<span class= 'yellow-date'>" + principal  +"</span>" +",\<br\>at an interest rate of "+ "<span class='yellow-date'>" + rate + "%"+ "</span>" +"\<br\>You will receive an amount of "+ "<span class='yellow-date'>" + interest + "</span>" +",\<br\>in the year "+ "<span class='yellow-date'>" + year + "</span>" +"\<br\>"
+
+}
